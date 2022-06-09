a04f841a1646169fbb23bccb2d551b824db34c3c
vg22 <vishwasgautam09@gmail.com>
2022-06-09 13:13:56 -0400

    install_software.ps1
    initial

---
+++ install_software.ps1
@@ -0,0 +1,3 @@
+Invoke-WebRequest https://github.com/Azure/azure-documentdb-datamigrationtool/releases/download/1.8.3/azure-documentdb-datamigrationtool-1.8.3.zip -OutFile C:\Users\adminuser\Desktop\datamigrationtool.zip
+Expand-Archive C:\Users\adminuser\Desktop\datamigrationtool.zip -DestinationPath C:\Users\adminuser\Desktop\
+Invoke-WebRequest https://github.com/microsoft/AzureStorageExplorer/releases/download/v1.21.3/Windows_StorageExplorer.exe   -OutFile  C:\Users\adminuser/Desktop\Windows_StorageExplorer.exe
