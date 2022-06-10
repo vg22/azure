0cda52b9f2417ac4cab2422669db2860bdfcb175
vg22 <vishwasgautam09@gmail.com>
2022-06-09 21:46:37 -0400

    Update install_software.ps1

--- install_software.ps1
+++ install_software.ps1
@@ -1,6 +1,6 @@
-Invoke-WebRequest https://github.com/Azure/azure-documentdb-datamigrationtool/releases/download/1.8.3/azure-documentdb-datamigrationtool-1.8.3.zip -OutFile C:\Packages\datamigrationtool.zip
+Invoke-WebRequest -UseBasicParsing https://github.com/Azure/azure-documentdb-datamigrationtool/releases/download/1.8.3/azure-documentdb-datamigrationtool-1.8.3.zip -OutFile C:\Packages\datamigrationtool.zip
 Expand-Archive C:\Packages\datamigrationtool.zip -DestinationPath C:\Users\adminuser.SRTHubCore\Desktop\
-Invoke-WebRequest https://github.com/microsoft/AzureStorageExplorer/releases/download/v1.21.3/Windows_StorageExplorer.exe   -OutFile  C:\Users\adminuser.SRTHubCore\Desktop\Windows_StorageExplorer.exe
+Invoke-WebRequest -UseBasicParsing https://github.com/microsoft/AzureStorageExplorer/releases/download/v1.21.3/Windows_StorageExplorer.exe   -OutFile  C:\Users\adminuser.SRTHubCore\Desktop\Windows_StorageExplorer.exe
 Set-ItemProperty -Path "HKLM:\\System\\CurrentControlSet\\Control\\Lsa\\FipsAlgorithmPolicy" -Name "Enabled" -Value 1
 Restart-Service W32Time
 w32tm /config /manualpeerlist:time.nist.gov /syncfromflags:manual /reliable:yes /update
